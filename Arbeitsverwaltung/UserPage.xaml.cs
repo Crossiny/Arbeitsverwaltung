3449218b33084af369d7c42dcc679e8f34514950
Crossiny <christoph@r-schimpf.de>
2016-07-03 11:58:26 +0200

    Shift und Break Methoden
    start und stop

--- Arbeitsverwaltung/UserPage.xaml.cs
+++ Arbeitsverwaltung/UserPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.Windows;
 using System.Windows.Controls;
+using Server.Database;
 
 namespace Arbeitsverwaltung
 {
@@ -9,6 +11,9 @@ namespace Arbeitsverwaltung
     /// </summary>
     public partial class UserPage : Page
     {
+        private Shift _shift;
+        private Break _break;
+
         public UserPage()
         {
             InitializeComponent();
@@ -16,22 +21,68 @@ namespace Arbeitsverwaltung
 
         private void StartShiftButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_shift == null)
+            {
+                _shift.StartTime = DateTime.Now;
+
+                MainWindow.PrintStatus("Shift is started.");
+            }
+            else if (_shift != null)
+            {
+                MainWindow.PrintStatus("Shift can't started because you already having a Shift.");
+            }
         }
 
         private void StopShiftButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_shift != null)
+            {
+                _shift.EndTime = DateTime.Now;
+
+                MainWindow.PrintStatus("Shift is stopped.");
+            }
+            else if (_shift == null)
+            {
+                MainWindow.PrintStatus("You can't stop a Shift before you have no Shift.");
+            }
         }
 
         private void StartBreakButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_break == null && _shift != null)
+            {
+                _break.StartTime = DateTime.Now;
+                MainWindow.PrintStatus("Break is started.");
+            }
+            else if (_break != null)
+            {
+                MainWindow.PrintStatus("Break can't started because you already having a Break.");
+            }
+            else if (_shift == null)
+            {
+                MainWindow.PrintStatus("You can't start a break before you have no Shift.");
+            }
+
         }
 
         private void StopBreakButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (_break != null && _shift != null)
+            {
+                _break.EndTime = DateTime.Now;
+                _shift.Breaks.Add(_break);
+                _break = null;
+
+                MainWindow.PrintStatus("Break is stopped.");
+            }
+            else if (_break == null)
+            {
+                MainWindow.PrintStatus("You can't stop a break before you have no Break.");
+            }
+            else if (_shift == null)
+            {
+                MainWindow.PrintStatus("You can't stop a break before you have no Shift and no Break.");
+            }
         }
     }
 }
